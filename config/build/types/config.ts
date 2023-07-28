f84bed922f795589a4416434e0ddc7da5265378c
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-07-28 14:51:00 +0300

    configurate webpack server

--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -8,6 +8,12 @@ export interface BuildPaths {
     html: string;
 }
 
+
+export interface BuildEnv {
+    mode: BuildMode,
+    port: number
+}
+
 export interface BuildOptions {
     mode : BuildMode;
     paths: BuildPaths;
