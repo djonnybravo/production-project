93bd292c13245d65311f9a5aa82ca13059d59866
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-07-28 16:58:07 +0300

    webpack css config

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,7 +2,7 @@ import webpack from "webpack";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import {BuildOptions} from "./types/config";
 
-export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
  //Если не исопользовать тайпскрипт, то нужно подключить еще babel-loader
  //Typescript уже умеет в React
 
@@ -17,12 +17,18 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
         test: /\.s[ac]ss$/i,
             use: [
         // Creates `style` nodes from JS strings
-        options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+        isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
         // Translates CSS into CommonJS
                 {
                     loader: "css-loader",
                     options: {
-                        modules: true
+                        modules: {
+                            auto: (resPath: string) => Boolean(resPath.includes('.module')),
+                            localIdentName: isDev
+                                ? "[path][name]__[local]--[hash:base64:8]"
+                                : '[hash:base64:8]'
+                        },
+
                     }
                 },
         // Compiles Sass to CSS
