01188a576744de000bc035ac3c2ffb64e5daabdc
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-07-28 16:49:20 +0300

    webpack css config

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Counter} from "./components/counter";
+import './index.scss'
+
+const App = () => {
+    return (
+        <div className={'App'}>
+            asdasdasd
+            <Counter/>
+        </div>
+    );
+};
+
+export default App;
