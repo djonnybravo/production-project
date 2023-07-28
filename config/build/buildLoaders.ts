78552bfcfc8fa8627fe2c9ac3e120d686c3e89bf
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-07-28 15:15:00 +0300

    install react + react-dom

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,8 @@
 import webpack from "webpack";
 
 export function buildLoaders(): webpack.RuleSetRule[] {
+ //Если не исопользовать тайпскрипт, то нужно подключить еще babel-loader
+ //Typescript уже умеет в React
 
     const typescriptLoader = {
         test: /\.tsx?$/,
