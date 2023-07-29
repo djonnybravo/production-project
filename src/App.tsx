502ad2cfb3ad0467d47b36dfeaae83f16d567c89
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-07-29 18:24:00 +0300

    add Code-Splitting for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import './index.scss'
-import {Route, Routes} from "react-router-dom";
+import {Link, Route, Routes} from "react-router-dom";
+
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 
 const App = () => {
     return (
         <div className={'App'}>
+            <div>
+              <Link to={'/'}>Главная</Link>
+              <Link to={'/about'}>О сайте</Link>
+            </div>
             <Routes>
-                <Route path={"/"} />
-                <Route path={"/about"} />
+                <Route path={"/"} element={<MainPageAsync/>} />
+                <Route path={"/about"} element={<AboutPageAsync/>} />
             </Routes>
         </div>
     );
