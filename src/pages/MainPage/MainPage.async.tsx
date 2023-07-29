b8a00502dcdf0d0636818bf16e4cbd9b18a74cb4
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-07-29 18:32:27 +0300

    add Code-Splitting for pages

--- src/pages/MainPage/MainPage.async.tsx
+++ src/pages/MainPage/MainPage.async.tsx
@@ -1,3 +1,8 @@
 import {lazy} from "react";
 
-export const MainPageAsync = lazy(() => import('./MainPage'));
+export const MainPageAsync = lazy(() => new Promise(res => {
+    // @ts-ignore
+    // В РЕАЛЬНЫХ ПРОЕКТАХ, ТАК НЕ ДЕЛАЕМ. СДЕЛАНО ДЛЯ ДЕМОНСТРАЦИИ ЗАГРУЗКИ ЧАНКОВ
+    setTimeout(() => res(import('./MainPage')), 1500)
+}));
+
