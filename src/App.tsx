ea89934fcd9d3d9eb30ce31fe7a7348c78e32897
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-07-29 17:58:27 +0300

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import {Counter} from "./components/counter";
 import './index.scss'
+import {Route, Routes} from "react-router-dom";
 
 const App = () => {
     return (
         <div className={'App'}>
-            asdasdasd
-            <Counter/>
+            <Routes>
+                <Route path={"/"} />
+                <Route path={"/about"} />
+            </Routes>
         </div>
     );
 };
