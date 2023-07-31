0823afcb312d2e58d591faf911147e3b336e776b
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-07-31 21:34:23 +0300

    add toggle themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import React, {Suspense} from 'react';
+import React, {Suspense, useState} from 'react';
 import './styles/index.scss'
 import {Link, Route, Routes} from "react-router-dom";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 
+export enum Theme {
+    LIGHT = 'light',
+    DARK = 'dark'
+}
+
 const App = () => {
+    const [theme, setTheme] = useState<Theme>(Theme.LIGHT)
+    const toggleTheme = () => {
+        setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK)
+    }
     return (
-        <div className={'app dark'}>
+        <div className={`app ${theme}`}>
             <div>
-                <Link to={'/'}>Главная</Link>
-                <Link to={'/about'}>О сайте</Link>
+                <button onClick={toggleTheme}>SWITCH THEME</button>
             </div>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
                     <Route path={"/"} element={<MainPageAsync/>}/>
