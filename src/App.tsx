4210bb670d891cc363d67f14604b5ffb89753f49
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-07-31 22:01:28 +0300

    create custom hook useTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
-import React, {Suspense, useState} from 'react';
+import React, {Suspense, useContext, useState} from 'react';
 import './styles/index.scss'
 import {Link, Route, Routes} from "react-router-dom";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {useTheme} from "./theme/useTheme";
 
-export enum Theme {
-    LIGHT = 'light',
-    DARK = 'dark'
-}
 
 const App = () => {
-    const [theme, setTheme] = useState<Theme>(Theme.LIGHT)
-    const toggleTheme = () => {
-        setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK)
-    }
+    const {theme, toggleTheme} = useTheme()
     return (
         <div className={`app ${theme}`}>
             <div>
