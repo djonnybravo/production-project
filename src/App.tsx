9bc674254bb4fd0db73bd9a279b0442008736709
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-07-31 20:53:40 +0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, {Suspense} from 'react';
-import './index.scss'
+import './styles/index.scss'
 import {Link, Route, Routes} from "react-router-dom";
 
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
