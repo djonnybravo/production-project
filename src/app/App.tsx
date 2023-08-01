bf529cff551af7edfb0ea875c1c450c865202219
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-08-01 21:10:37 +0300

    change structure of project(FSD)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,15 @@
 import React, {Suspense, useContext, useState} from 'react';
 import './styles/index.scss'
 import {Link, Route, Routes} from "react-router-dom";
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
+import {AboutPageAsync} from "../pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "../pages/MainPage/MainPage.async";
+import {useTheme} from "./providers/ThemeProvider/lib/useTheme";
+import {classNames} from "../helpers/classNames/classNames";
 
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
+
     return (
         <div className={classNames('app', {}, [theme])}>
             <div>
