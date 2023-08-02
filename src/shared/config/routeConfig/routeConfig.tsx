79bda0986a787e439afeead9559779425e75bbc4
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-08-02 22:34:41 +0300

    create route config

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,5 +1,6 @@
 import {RouteProps} from "react-router-dom";
-import MainPage from "pages/MainPage";
+import {MainPage} from "pages/MainPage";
+import {AboutPage} from "pages/AboutPage";
 
 export enum AppRoutes {
     MAIN = 'main',
@@ -18,5 +19,9 @@ export const routeConfig: Record<AppRoutes, RouteProps> = {
     [AppRoutes.MAIN]: {
         path: RoutePath.main,
         element: <MainPage/>
+    },
+    [AppRoutes.ABOUT]: {
+        path: RoutePath.about,
+        element: <AboutPage/>
     }
 }
