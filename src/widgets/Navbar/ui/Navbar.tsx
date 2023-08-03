50a70e019cb8e60a01d7e511a55fd72281f1ea69
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-08-03 16:45:37 +0300

    fix: NavBar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {Link} from "react-router-dom";
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from './Navbar.module.scss'
+import {AppLink} from "shared/ui/AppLink/AppLink";
 interface NavbarProps {
     className?: string,
 
@@ -10,8 +10,8 @@ export const Navbar = ({className}: NavbarProps) => {
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
             <div className={cls.links}>
-                <Link to={'/'} className={cls.mainLink}>Главная</Link>
-                <Link to={'/about'}>О сайте</Link>
+                <AppLink to={'/'} className={cls.mainLink}>Главная</AppLink>
+                <AppLink to={'/about'}>О сайте</AppLink>
             </div>
         </div>
     );
