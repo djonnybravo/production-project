73a5a471a7861e5bdbf66f9fdca141d4386284ab
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-08-03 16:05:45 +0300

    feat: navBar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './styles/index.scss'
-import {Link} from "react-router-dom";
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
 import {AppRouter} from "app/providers/router";
