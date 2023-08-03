928b92273219b58a7d31e35e44312875975cb9b7
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-08-03 16:04:08 +0300

    fix: fix CN args

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -4,7 +4,7 @@ type Mods = Record<string, boolean | string>
 export function classNames(className:string, mods: Mods = {}, additional: string[] = []):string {
     return [
         className,
-        ...additional,
+        ...additional.filter(Boolean),
         ...Object.entries(mods)
             .filter(([className,value]) => Boolean(value))
             .map(([className, value]) => className)
