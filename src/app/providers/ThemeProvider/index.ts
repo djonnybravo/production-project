12496044000f8fc8acfc669fb9bb3d5f14422ec3
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-08-06 22:10:26 +0300

    fix: fix public API

--- src/app/providers/ThemeProvider/index.ts
+++ src/app/providers/ThemeProvider/index.ts
@@ -1,10 +1,12 @@
 import ThemeProvider from "./ui/ThemeProvider";
 import {useTheme} from "./lib/useTheme";
+import {Theme} from "app/providers/ThemeProvider/lib/ThemeContext";
 
 
 
 export
 {
     ThemeProvider,
-    useTheme
+    useTheme,
+    Theme
 }
