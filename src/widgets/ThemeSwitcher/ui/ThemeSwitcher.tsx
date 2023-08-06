c7379fd7d638c4b2cdb2ef10275b45ed61d15cb8
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-08-06 21:19:31 +0300

    feat: add themeswitcher icons

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,8 +1,9 @@
-
 import cls from './ThemeSwitcher.module.scss'
 import React from "react";
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
+import LightThemeIcon from 'shared/assets/icons/ThemeSwitcherIcons/theme-light.svg'
+import DarkThemeIcon from 'shared/assets/icons/ThemeSwitcherIcons/theme-dark.svg'
 interface ThemeSwitcherProps {
     className?: string,
 
