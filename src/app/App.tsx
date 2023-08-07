a6233efc6f17ec4e07cbdc3bfb4dda5ef4182b09
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-08-07 18:46:45 +0300

    feat: config i18n

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import './styles/index.scss'
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
 import {Sidebar} from "widgets/Sidebar";
+import {useTranslation} from "react-i18next";
 
+const Component = () => {
+
+    const { t, i18n } = useTranslation();
+    const toggle = () => {
+        i18n.changeLanguage(i18n.language === "ru" ? 'en' : 'ru')
+    }
+    return <div>
+        <button onClick={toggle}>{t("Перевести")}</button>
+        <h1>{t('Тестовый пример')}</h1>
+
+    </div>
+}
 
 const App = () => {
     const {theme} = useTheme()
 
     return (
         <div className={classNames('app', {}, [theme])}>
+            <Suspense fallback='' >
+                <Component/>
             <Navbar/>
             <div className={'content-page'}>
                 <Sidebar/>
                 <AppRouter/>
 
             </div>
+            </Suspense>
+
         </div>
     );
 };
