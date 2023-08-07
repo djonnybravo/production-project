87a7ab23e74bf9868b5a812a18b2ab3826fdd1c4
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-08-07 17:48:39 +0300

    feat: css

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,10 +1,13 @@
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from './Sidebar.module.scss'
-import {useState} from "react";
+import React, {useState} from "react";
+import {ThemeSwitcher} from "widgets/ThemeSwitcher";
+
 interface SidebarProps {
     className?: string,
 
 }
+
 export const Sidebar = ({className}: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false)
 
@@ -15,7 +18,10 @@ export const Sidebar = ({className}: SidebarProps) => {
         <div
             className={classNames(cls.Sidebar, {[cls.collapsed]: collapsed}, [className])}
         >
-        <button onClick={onToggle}>toggle</button>
+            <button onClick={onToggle}>toggle</button>
+            <div className={cls.switchers}>
+                <ThemeSwitcher/>
+            </div>
         </div>
     );
 };
