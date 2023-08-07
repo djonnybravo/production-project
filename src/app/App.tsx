3c5c54f68dfa9a3d03dc47ab7831061373e39464
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-08-07 19:36:33 +0300

    feat: add LangSwitcher

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,15 +9,6 @@ import {useTranslation} from "react-i18next";
 
 const Component = () => {
 
-    const { t, i18n } = useTranslation();
-    const toggle = () => {
-        i18n.changeLanguage(i18n.language === "ru" ? 'en' : 'ru')
-    }
-    return <div>
-        <button onClick={toggle}>{t("Перевести")}</button>
-        <h1>{t('Тестовый пример')}</h1>
-
-    </div>
 }
 
 const App = () => {
@@ -26,12 +17,10 @@ const App = () => {
     return (
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback='' >
-                <Component/>
             <Navbar/>
             <div className={'content-page'}>
                 <Sidebar/>
                 <AppRouter/>
-
             </div>
             </Suspense>
 
