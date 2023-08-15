bafe4640c60cb8fd45df89a50110aab81c23e92c
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-08-15 22:22:42 +0300

    feat: add 404 page in routeConfig

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,14 +1,17 @@
 import { RouteProps } from 'react-router-dom';
 import { MainPage } from 'pages/MainPage';
 import { AboutPage } from 'pages/AboutPage';
+import { NotFoundPage } from 'pages/NotFoundPage';
 
 export enum AppRoutes {
     MAIN = 'main',
     ABOUT = 'about',
+    NOT_FOUND = 'not_found',
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
     [AppRoutes.ABOUT]: '/about',
+    [AppRoutes.NOT_FOUND]: '/not_found',
     [AppRoutes.MAIN]: '/',
 };
 
@@ -20,5 +23,9 @@ export const routeConfig: Record<AppRoutes, RouteProps> = {
     [AppRoutes.ABOUT]: {
         path: RoutePath.about,
         element: <AboutPage />,
+    },
+    [AppRoutes.NOT_FOUND]: {
+        path: RoutePath.not_found,
+        element: <NotFoundPage />,
     },
 };
