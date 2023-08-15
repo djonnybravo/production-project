9f24771667f2c48cd42e60a20688bfecb374df05
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-08-15 22:35:24 +0300

    feat: add PageLoader

---
+++ src/shared/ui/PageLoader/PageLoader.tsx
@@ -0,0 +1,17 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './PageLoader.module.scss';
+
+interface PageLoaderProps {
+    className?: string,
+
+}
+export const PageLoader = ({ className }: PageLoaderProps) => (
+    <div className={classNames(cls.PageLoader, {}, [className])}>
+        <div className="lds-ellipsis">
+            <div />
+            <div />
+            <div />
+            <div />
+        </div>
+    </div>
+);
