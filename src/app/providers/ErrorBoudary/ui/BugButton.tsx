8f10535bbc36ed3166520a606a2bbab020aba96a
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-08-16 18:58:53 +0300

    feat: create BugButton for test ErrorBoundary

--- src/app/providers/ErrorBoudary/ui/BugButton.tsx
+++ src/app/providers/ErrorBoudary/ui/BugButton.tsx
@@ -1,23 +1,25 @@
-import { classNames } from 'shared/lib/classNames/classNames';
 import { Button } from 'shared/ui/Button/Button';
 import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
-interface BugButtonProps {
-    className?: string,
-
-}
-export const BugButton = ({ className }: BugButtonProps) => {
+// Компонент для тестирования ErrorBoundary
+export const BugButton = () => {
     const [error, setError] = useState(false);
+    const { t } = useTranslation();
 
-    const throwNewEror = () => setError(true);
+    const onThrow = () => setError(true);
 
     useEffect(() => {
-        throw new Error();
+        if (error) {
+            throw new Error();
+        }
     }, [error]);
+
     return (
-        // eslint-disable-next-line i18next/no-literal-string
-        <Button onClick={throwNewEror}>
-            Throw error
+        <Button
+            onClick={onThrow}
+        >
+            {t('throw error')}
         </Button>
     );
 };
