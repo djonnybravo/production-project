cad51cdcc20fb986121d4fd4d534b421236a9b5c
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-08-16 18:17:03 +0300

    feat: add ErrorBoundary Provider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import './styles/index.scss';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
@@ -8,7 +8,9 @@ import { Sidebar } from 'widgets/Sidebar';
 
 function App() {
     const { theme } = useTheme();
+    useEffect(() => {
 
+    }, []);
     return (
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
