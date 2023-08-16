b24418f0871fffbe3558372fc4fdec6767c15cc9
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-08-16 18:27:11 +0300

    feat: add PageError to ErrorBoundary

--- src/app/providers/ErrorBoudary/ui/ErrorBoundary.tsx
+++ src/app/providers/ErrorBoudary/ui/ErrorBoundary.tsx
@@ -31,7 +31,11 @@ class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundarySta
             // You can render any custom fallback UI
 
             // eslint-disable-next-line i18next/no-literal-string
-            return <Suspense fallback=""><PageError /></Suspense>;
+            return (
+                <Suspense fallback="">
+                    <PageError />
+                </Suspense>
+            );
         }
 
         return children;
