9d68de0638e972a2cb60c09a69c365703ed67002
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-08-16 18:18:09 +0300

    feat: add ErrorBoundary Provider

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
 import './shared/config/i18n/i18n';
 import App from 'app/App';
+import { ErrorBoundary } from 'app/providers/ErrorBoudary';
 
 render(
     <BrowserRouter>
-        <ThemeProvider>
-            <App />
-        </ThemeProvider>
+        <ErrorBoundary>
+            <ThemeProvider>
+                <App />
+            </ThemeProvider>
+        </ErrorBoundary>
     </BrowserRouter>,
     document.getElementById('root'),
 );
