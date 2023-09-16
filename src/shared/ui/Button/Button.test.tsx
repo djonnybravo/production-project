4a2c09524b3c3491f612b6192bc7c4fcc78057ed
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-09-16 15:24:47 +0300

    feat: add second test case names)

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -2,12 +2,12 @@ import { render, screen } from '@testing-library/react';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
 
 describe('Button tests with React Testing library', () => {
-    test('with first param', () => {
+    test('Test render', () => {
         // eslint-disable-next-line i18next/no-literal-string
         render(<Button>TEST</Button>);
         expect(screen.getByText('TEST')).toBeInTheDocument();
     });
-    test('with first param', () => {
+    test('Test  clear theme', () => {
         // eslint-disable-next-line i18next/no-literal-string
         render(<Button theme={ThemeButton.CLEAR}>TEST</Button>);
         expect(screen.getByText('TEST')).toHaveClass('clear');
