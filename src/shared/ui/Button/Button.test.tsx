dec531d32214abeb043137a38f37f9a1bef602d4
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-09-16 15:22:25 +0300

    feat: add second test case

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,10 +1,15 @@
 import { render, screen } from '@testing-library/react';
-import { Button } from 'shared/ui/Button/Button';
+import { Button, ThemeButton } from 'shared/ui/Button/Button';
 
 describe('Button tests with React Testing library', () => {
     test('with first param', () => {
         // eslint-disable-next-line i18next/no-literal-string
         render(<Button>TEST</Button>);
         expect(screen.getByText('TEST')).toBeInTheDocument();
+    });
+    test('with first param', () => {
+        // eslint-disable-next-line i18next/no-literal-string
+        render(<Button theme={ThemeButton.CLEAR}>TEST</Button>);
+        expect(screen.getByText('TEST')).toHaveClass('clear');
     });
 });
