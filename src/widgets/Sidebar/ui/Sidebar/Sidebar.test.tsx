87ce76facbf01fbd1cc6c1730cfea4a852f33e3f
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-09-16 15:49:23 +0300

    feat: add second test case

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,11 +1,18 @@
-import { Sidebar } from 'widgets/Sidebar';
-import { render, screen } from '@testing-library/react';
-import { withTranslation } from 'react-i18next';
+import { fireEvent, screen } from '@testing-library/react';
+import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
+import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation';
 
-describe('Sidebar tests', () => {
-    test('', () => {
-        const SideBarWithTranslation = withTranslation()(Sidebar);
-        render(<SideBarWithTranslation />);
+describe('Sidebar', () => {
+    test('sidebar test render', () => {
+        renderWithTranslation(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
+    });
+
+    test('test toggle', () => {
+        renderWithTranslation(<Sidebar />);
+        const toggleBtn = screen.getByTestId('sidebar-toggle');
+        expect(screen.getByTestId('sidebar')).toBeInTheDocument();
+        fireEvent.click(toggleBtn);
+        expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
     });
 });
