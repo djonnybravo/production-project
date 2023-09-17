1eb130a94b512f81114eb5de2fb26e5e058a4e98
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-09-17 18:44:41 +0300

    feat: add helper to create css loader for webpack and sb build

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,6 +1,7 @@
 import webpack from 'webpack';
 import path from 'path';
 import { BuildPaths } from '../build/types/config';
+import { buildCssLoader } from '../build/loaders/buildCssLoader';
 
 export default ({ config }: {config: webpack.Configuration}) => {
     const paths: BuildPaths = {
@@ -11,5 +12,6 @@ export default ({ config }: {config: webpack.Configuration}) => {
     };
     config.resolve.modules.push(paths.src);
     config.resolve.extensions.push('.ts', '.tsx');
+    config.module.rules.push(buildCssLoader(true));
     return config;
 };
