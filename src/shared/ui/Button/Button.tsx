886cb916d36a6b3f3b816889d3bf32db35cea34f
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-09-17 19:51:56 +0300

    feat: add new THEME in button

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -4,6 +4,7 @@ import cls from './Button.module.scss';
 
 export enum ThemeButton {
     CLEAR = 'clear',
+    OUTLINE = 'ouline'
 
 }
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
